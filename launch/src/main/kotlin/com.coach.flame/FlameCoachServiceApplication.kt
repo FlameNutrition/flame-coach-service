a54c55d4a43f5eb652b949e4a3d459924b6bdb2e
nbentoneves <nbento.neves@gmail.com>
2021-01-26 19:26:17 +0000

    Create flame-coach-service structure

---
+++ launch/src/main/kotlin/com.coach.flame/FlameCoachServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.coach.flame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlameCoachServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlameCoachServiceApplication>(*args)
+}
