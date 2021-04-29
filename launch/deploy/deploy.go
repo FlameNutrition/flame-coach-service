3a2c1eae8d04c999a93a9c83371e9951a44b93ae
nbentoneves <nbento.neves@gmail.com>
2021-04-29 15:19:42 +0100

    #29 - Update deploy script

--- launch/deploy/deploy.go
+++ launch/deploy/deploy.go
@@ -16,7 +16,7 @@ func main() {
 	version := flag.String("version", "", "Application version")
 	deploy := flag.Bool("deploy", false, "Deploy application")
 	redeploy := flag.Bool("redeploy", false, "Redeploy application")
-	dryRun := flag.Bool("dryRun", false, "Simulating zip and deploy application")
+	dryRun := flag.Bool("dryRun", false, "Simulating commands to deploy application")
 
 	flag.Parse()
 
@@ -24,25 +24,60 @@ func main() {
 
 		applicationPackage := application + "v" + *version + ".zip"
 
+		gitFetch := exec.Command("git", "fetch", "--all", "--tags")
+		gitCheckoutTag := exec.Command("git", "checkout", fmt.Sprintf("tags/v%s", *version), "-b", fmt.Sprintf("release/%s", *version))
+
 		if *deploy {
+			executeCommand(*gitFetch, "git fetch", *dryRun)
+			executeCommand(*gitCheckoutTag, "git checkout tag version", *dryRun)
+
+			mvnCleanInstall := exec.Command("mvn", "clean", "install", "-f", "../../pom.xml", "-Dskip.component.tests=false")
+
+			if !*dryRun {
+				out, err := mvnCleanInstall.Output()
+				if err != nil {
+					cleanReleaseBranch(*version, *dryRun)
+					log.Fatalf("%s: %v", "mvn clean install", err)
+				}
+				log.Printf("%s", out)
+			} else {
+				log.Printf("execute cmd: %v", mvnCleanInstall.String())
+			}
 
 			copyCmd(*dryRun)
-			//pushS3Cmd(application, awsS3Bucket, applicationPackage, *dryRun)
-			//deployCmd(application, awsS3Bucket, applicationPackage, *dryRun)
+			pushS3Cmd(application, awsS3Bucket, applicationPackage, *version, *dryRun)
+			deployCmd(application, awsS3Bucket, applicationPackage, *version, *dryRun)
 
-		}
-
-		if *redeploy {
-
-			deployCmd(application, awsS3Bucket, applicationPackage, *dryRun)
-
+			cleanReleaseBranch(*version, *dryRun)
+		} else if *redeploy {
+			deployCmd(application, awsS3Bucket, applicationPackage, *version, *dryRun)
+		} else {
+			log.Fatal("Please use --deploy or --redeploy options")
 		}
 	} else {
 		log.Fatal("Please define a software version to deploy")
 	}
 }
 
-func pushS3Cmd(applicationName string, s3BucketName string, applicationPackage string, dryRun bool) {
+func cleanReleaseBranch(version string, dryRun bool) {
+	log.Print("Cleaning git release branch...")
+	executeCommand(*exec.Command("git", "checkout", "master"), "git checkout master", dryRun)
+	executeCommand(*exec.Command("git", "branch", "-d", fmt.Sprintf("release/%s", version)), "git delete branch release", dryRun)
+}
+
+func executeCommand(command exec.Cmd, failedMsg string, dryRun bool) {
+	if !dryRun {
+		out, err := command.Output()
+		if err != nil {
+			log.Fatalf("%s: %v", failedMsg, err)
+		}
+		log.Printf("%s", out)
+	} else {
+		log.Printf("execute cmd: %v", command.String())
+	}
+}
+
+func pushS3Cmd(applicationName string, s3BucketName string, applicationPackage string, version string, dryRun bool) {
 	s3Cmd := exec.Command("aws", "deploy", "push",
 		"--application-name", applicationName,
 		"--s3-location", fmt.Sprintf("s3://%s/%s", s3BucketName, applicationPackage),
@@ -52,13 +87,14 @@ func pushS3Cmd(applicationName string, s3BucketName string, applicationPackage s
 	} else {
 		out, err := s3Cmd.Output()
 		if err != nil {
+			cleanReleaseBranch(version, dryRun)
 			log.Fatalf("Failed when try to push to S3, %v", err)
 		}
 		log.Printf("%s", out)
 	}
 }
 
-func deployCmd(applicationName string, s3BucketName string, applicationPackage string, dryRun bool) {
+func deployCmd(applicationName string, s3BucketName string, applicationPackage string, version string, dryRun bool) {
 	deployCmd := exec.Command("aws", "deploy", "create-deployment",
 		"--application-name", applicationName,
 		"--deployment-group-name", "production",
@@ -68,12 +104,14 @@ func deployCmd(applicationName string, s3BucketName string, applicationPackage s
 	} else {
 		out, err := deployCmd.Output()
 		if err != nil {
+			cleanReleaseBranch(version, dryRun)
 			log.Fatalf("Failed when try to push to create-deployment, %v", err)
 		}
 
 		var jsonResult map[string]interface{}
 		err = json.Unmarshal(out, &jsonResult)
 		if err != nil {
+			cleanReleaseBranch(version, dryRun)
 			log.Fatalf("deploymentId json parsing failed, %v", err)
 		}
 
--- launch/scripts/start-application.sh
+++ launch/scripts/start-application.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd /etc/app || exit 1
-java -Dspring.datasource.password=$FLAME_COACH_DB_PASSWORD -Dlog4j.configurationFile=log4j2.xml -jar /etc/app/flame-coach.jar > /dev/null &
+java -Dspring.datasource.password=$FLAME_COACH_DB_PASSWORD -Dlog4j.configurationFile=log4j2.xml -Dflamecoach.rest.debug.enable=false -jar \
+	/etc/app/flame-coach.jar > /dev/null 2> /dev/null < /dev/null &
