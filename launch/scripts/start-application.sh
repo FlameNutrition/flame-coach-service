81da390b14af05234f6e27ff2df756de2047c663
nbentoneves <nbento.neves@gmail.com>
2021-04-30 09:15:25 +0100

    Implement SSL cert for spring boot

--- launch/scripts/start-application.sh
+++ launch/scripts/start-application.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd /etc/app || exit 1
-java -Dspring.datasource.password=$FLAME_COACH_DB_PASSWORD -Dlog4j.configurationFile=log4j2.xml -Dflamecoach.rest.debug.enable=false -jar \
+java -Dspring.datasource.password=$FLAME_COACH_DB_PASSWORD -Dserver.ssl.key-store=classpath:cert/flame-coach.p12 \
+  -Dserver.ssl.key-store-password=$FLAME_COACH_SSL_PASSWORD -Dlog4j.configurationFile=log4j2.xml -Dflamecoach.rest.debug.enable=false -jar \
 	/etc/app/flame-coach.jar > /dev/null 2> /dev/null < /dev/null &
