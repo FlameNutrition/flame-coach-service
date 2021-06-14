1b7ce82cd4b78d11b725bf76456212216f646614
nbentoneves <nbento.neves@gmail.com>
2021-06-14 16:46:19 +0100

    Fix start-application.sh script with smtp props

--- launch/scripts/start-application.sh
+++ launch/scripts/start-application.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 cd /etc/app || exit 1
-java -Dspring.datasource.password=$FLAME_COACH_DB_PASSWORD -Dserver.ssl.key-store=classpath:cert/flame-coach.p12 \
-  -Dserver.ssl.key-store-password=$FLAME_COACH_SSL_PASSWORD -Dlog4j.configurationFile=log4j2.xml -Dflamecoach.rest.debug.enable=false -jar \
-  /etc/app/flame-coach.jar > /dev/null 2> /dev/null < /dev/null &
+java -Dspring.datasource.password=$FLAME_COACH_DB_PASSWORD \
+  -Dserver.ssl.key-store=classpath:cert/flame-coach.p12 \
+  -Dserver.ssl.key-store-password=$FLAME_COACH_SSL_PASSWORD \
+  -Dlog4j.configurationFile=log4j2.xml \
+  -Dflamecoach.rest.debug.enable=false \
+  -Dspring.mail.username=$FLAME_COACH_SMTP_USERNAME \
+  -Dspring.mail.password=$FLAME_COACH_SMTP_PASSWORD \
+  -Dflamecoach.rest.debug.enable=false \
+  -jar /etc/app/flame-coach.jar > /dev/null 2> /dev/null < /dev/null &
