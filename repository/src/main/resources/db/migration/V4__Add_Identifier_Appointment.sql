47b19f2ae80418f88d37e29f2d67c944352ef090
nbentoneves <nbento.neves@gmail.com>
2021-07-21 12:55:05 +0100

    Fix Appointment migration script

--- repository/src/main/resources/db/migration/V4__Add_Identifier_Appointment.sql
+++ repository/src/main/resources/db/migration/V4__Add_Identifier_Appointment.sql
@@ -1,4 +1,4 @@
 alter table Appointment
-add column identifier binary(255) not null;
+add column identifier varchar(255) not null;
 
 alter table Appointment add constraint UK_n3me5gxv0r2ttq6nbptvkvvc2 unique (identifier);
